fd9d3b4cf30475861dcb3bb180afd76194754eba
Shivang Singh <shivangs2007@gmail.com>
2026-08-15 16:36:15 +0530

    book - congifguration

--- books_recommender/config/configuration.py
+++ books_recommender/config/configuration.py
@@ -28,6 +28,7 @@ from books_recommender.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
     DataTransformationConfig,
+    ModelRecommendationConfig,
     ModelTrainerConfig
 )
 from books_recommender.utils.utils import read_yaml_file
@@ -106,9 +107,27 @@ class AppConfiguration:
     def get_data_validation_config(self) -> DataValidationConfig:
 
         try:
+
             artifacts_dir = self.configs_info["artifacts_config"]["artifacts_dir"]
 
+            data_ingestion_config = self.configs_info["data_ingestion_config"]
+
             data_validation_config = self.configs_info["data_validation_config"]
+
+            books_csv_file = os.path.join(
+            data_ingestion_config["data_dir"],
+            data_ingestion_config["books_csv_file"]
+        )
+
+            users_csv_file = os.path.join(
+            data_ingestion_config["data_dir"],
+            data_ingestion_config["users_csv_file"]
+        )
+
+            ratings_csv_file = os.path.join(
+            data_ingestion_config["data_dir"],
+            data_ingestion_config["ratings_csv_file"]
+        )
 
             clean_data_file = os.path.join(
                 artifacts_dir,
@@ -116,8 +135,11 @@ class AppConfiguration:
             )
 
             response = DataValidationConfig(
-                clean_data_file=clean_data_file
-            )
+            books_csv_file=books_csv_file,
+            users_csv_file=users_csv_file,
+            ratings_csv_file=ratings_csv_file,
+            clean_data_file=clean_data_file
+        )
 
             logging.info(f"Data Validation Config: {response}")
 
@@ -130,35 +152,42 @@ class AppConfiguration:
     def get_data_transformation_config(self) -> DataTransformationConfig:
 
         try:
-            data_transformation_config = self.configs_info[
-                "data_transformation_config"
-            ]
+            artifacts_dir = self.configs_info["artifacts_config"]["artifacts_dir"]
+            data_validation_config = self.configs_info["data_validation_config"]
+            data_transformation_config = self.configs_info["data_transformation_config"]
 
-            final_rating_file = data_transformation_config[
-                "final_rating_file"
-            ]
+            clean_data_file = os.path.join(
+                artifacts_dir,
+                data_validation_config["clean_data_file"]
+        )
 
-            book_pivot_file = data_transformation_config[
-                "book_pivot_file"
-            ]
+            final_rating_file = os.path.join(
+                artifacts_dir,
+                data_transformation_config["final_rating_file"]
+        )
 
-            book_names_file = data_transformation_config[
-                "book_names_file"
-            ]
+            book_pivot_file = os.path.join(
+                artifacts_dir,
+                data_transformation_config["book_pivot_file"]
+        )
+
+            book_names_file = os.path.join(
+                artifacts_dir,
+                data_transformation_config["book_names_file"]
+        )
 
             response = DataTransformationConfig(
+                clean_data_file=clean_data_file,
                 final_rating_file=final_rating_file,
                 book_pivot_file=book_pivot_file,
                 book_names_file=book_names_file
-            )
+        )
 
             logging.info(f"Data Transformation Config: {response}")
-
             return response
 
         except Exception as e:
             raise AppException(e, sys) from e
-
 
     def get_model_trainer_config(self) -> ModelTrainerConfig:
 
@@ -171,13 +200,50 @@ class AppConfiguration:
 
             model_name = model_trainer_config["model_name"]
 
+            trained_model_path = os.path.join(
+                model_dir,
+                model_name
+            )
+
             response = ModelTrainerConfig(
                 model_dir=model_dir,
-                model_name=model_name
+                model_name=model_name,
+                trained_model_path=trained_model_path
             )
 
             logging.info(f"Model Trainer Config: {response}")
 
+            return response
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+
+    def get_recommendation_config(self) -> ModelRecommendationConfig:
+        try:
+            artifacts_dir = self.configs_info["artifacts_config"]["artifacts_dir"]
+            data_transformation = self.configs_info["data_transformation_config"]
+            model_trainer = self.configs_info["model_trainer_config"]
+
+            response = ModelRecommendationConfig(
+                book_names_file=os.path.join(
+                    artifacts_dir,
+                    data_transformation["book_names_file"]
+            ),
+                book_pivot_file=os.path.join(
+                    artifacts_dir,
+                    data_transformation["book_pivot_file"]
+            ),
+                final_rating_file=os.path.join(
+                    artifacts_dir,
+                    data_transformation["final_rating_file"]
+            ),
+                trained_model_path=os.path.join(
+                    model_trainer["model_dir"],
+                    model_trainer["model_name"]
+            )
+        )
+
+            logging.info(f"Recommendation Config: {response}")
             return response
 
         except Exception as e:
